b269b0450182dfaaa0091594b2fc5804205fb3d3
pirueto2004 <juliobell2014@gmail.com>
2020-10-22 21:12:49 -0400

    Updated Results.js

--- client/src/pages/Results/Results.js
+++ client/src/pages/Results/Results.js
@@ -86,7 +86,7 @@ class Results extends Component {
                   <a key={"" + index + book.id} href={book.volumeInfo.infoLink} target={this.state.target}>
                     {book.volumeInfo.title}
                   </a>
-                  <p>Written By {book.volumeInfo.authors[0]}</p>
+                  <p>Written By {book.volumeInfo.authors}</p>
                   <p>
                     <img align="left" style={{ paddingRight: 10 }} src={book.volumeInfo.imageLinks.smallThumbnail} alt="new" />
                     {book.volumeInfo.description}
@@ -100,7 +100,7 @@ class Results extends Component {
                     onClick={() =>
                       this.saveBook({
                         title: book.volumeInfo.title,
-                        author: book.volumeInfo.authors[0],
+                        author: book.volumeInfo.authors,
                         description: book.volumeInfo.description,
                         image: book.volumeInfo.imageLinks.smallThumbnail,
                         link: book.volumeInfo.infoLink,
